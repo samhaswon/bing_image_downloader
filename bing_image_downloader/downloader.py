22c7a6686f9019a2d91641fc99efd1cb45621d35
Samuel Howard <sahoward42@tntech.edu>
2023-10-17 21:43:23 -0500

    Added docstring and `img_size` usage

--- bing_image_downloader/downloader.py
+++ bing_image_downloader/downloader.py
@@ -1,4 +1,4 @@
-import os, sys
+import sys
 import shutil
 from pathlib import Path
 
@@ -8,16 +8,29 @@ except ImportError:  # Python 3
     from .bing import Bing
 
 
-def download(query, limit=100, output_dir='dataset', adult_filter_off=True, 
-force_replace=False, timeout=60, filter="", verbose=True):
+def download(query: str, limit=100, output_dir='dataset', adult_filter_off=True,
+             force_replace=False, timeout=60, img_filter=None, img_size=None, verbose=True):
+    """
+    Downloads a series of images from Bing according to the query and other parameters
+    :param query: String to query Bing for images of.
+    :param limit: Maximum number of images to download for the given query
+    :param output_dir: Directory to place the downloaded images in
+    :param adult_filter_off: whether to disable the Adult Content img_filter, or not (True disables it)
+    :param force_replace: Replaces the `output_dir` is already present
+    :param timeout: connection timeout in seconds
+    :param img_filter: Filter images by desired type: line|photo|clipart|gif|transparent
+    :param img_size: Minimum size of the images to download with `_` separating the width and height respectively (i.e.
+    `512_512`)
+    :param verbose: Whether to enable verbose logging
 
-    # engine = 'bing'
+    """
+    if img_filter is None:
+        img_filter = ""
     if adult_filter_off:
         adult = 'off'
     else:
         adult = 'on'
 
-    
     image_dir = Path(output_dir).joinpath(query).absolute()
 
     if force_replace:
@@ -32,9 +45,9 @@ force_replace=False, timeout=60, filter="", verbose=True):
     except Exception as e:
         print('[Error]Failed to create directory.', e)
         sys.exit(1)
-        
+
     print("[%] Downloading Images to {}".format(str(image_dir.absolute())))
-    bing = Bing(query, limit, image_dir, adult, timeout, filter, verbose)
+    bing = Bing(query, limit, image_dir, adult, timeout, img_filter, img_size, verbose)
     bing.run()
 
 
